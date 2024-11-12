08b9c381f1cf6e4b73be4a1ee7a0fba89591a886
JkamellV <kamell1025@hotmail.com>
2024-11-12 15:53:17 -0500

    subiendo proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import { Header } from './components/header';
 import './App.css';
+import home from './imagenes/Gaslur.png';
 
 function App() {
   return (
     <>
-      <Header>
-        <p>hola</p>
-      </Header>
-      <div className="contenedor"></div>
-      <img src="" alt="" />
-      <input type="submit" disabled />
-      <video src=""></video>
-      <a href=""></a>
-      <button disabled={true}>hola</button>
+      <header>
+        <div className="content">
+          <div className="image">
+            <img src={home} alt="" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">HOME</a>
+              </li>
+              <li>
+                <a href="#">MY PROFILE</a>
+              </li>
+              <li>
+                <a href="#">ACTIVITY</a>
+              </li>
+              <li>
+                <a href="#">HOT IT WORKS</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="login">
+            <div></div>
+          </div>
+        </div>
+      </header>
+      <div className="content1">
+        <div className="content2">
+          <h1 className="tipoletras">
+            Discover, collect, <br /> and charity in <br /> extraordinary NFT{' '}
+            <br />
+            marketplace
+          </h1>
+          <div className="Subcontenedor">
+            <p>
+              In aenean posuere lorem risus nec. Tempor <br /> tincidunt aenean
+              purus purus vestibulum nibh mi <br />
+              venenatis
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="contenedorimg">
+        <p> hola </p>
+        <div className="subcontenedorimg"></div>
+      </div>
     </>
   );
 }
